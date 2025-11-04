0229320a70fc5c8b9ea95b19366b2682b2b90afa
primedfury99-create <primedfury99@gmail.com>
2025-11-04 08:34:25 +0200

    Update ping-urls.js

--- scripts/ping-urls.js
+++ scripts/ping-urls.js
@@ -1,7 +1,17 @@
 // ping-urls.js
 
 const URLS_FROM_ENV = [process.env.API_URL_1, process.env.API_URL_2].filter(Boolean);
-const URLs = URLS_FROM_ENV.length ? URLS_FROM_ENV : [
+const URLs =  [
+  'https://boringaswell.netlify.app/api/all-get',
+  'https://boringaswell.netlify.app/api/all-get-2',
+  'https://boringaswell.netlify.app/api/all-get-2-old',
+    "https://luminous-boba-b62ad5.netlify.app/.netlify/functions/all-get2-background",
+  "https://luminous-boba-b62ad5.netlify.app/.netlify/functions/all-get2-oldway-background",
+    'https://boringaswell.netlify.app/api/all-get',
+  'https://boringaswell.netlify.app/api/all-get-2',
+  'https://boringaswell.netlify.app/api/all-get-2-old',
+      "https://luminous-boba-b62ad5.netlify.app/.netlify/functions/all-get2-background",
+  "https://luminous-boba-b62ad5.netlify.app/.netlify/functions/all-get2-oldway-background",
 ];
 
 // Requires Node 18+ (global fetch + AbortController)
